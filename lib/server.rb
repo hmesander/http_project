a34916d54d3643c281ca8436f2a517b249139e2b
hmesander <haleymesander@gmail.com>
2018-02-11 16:27:18 -0700

    Creates runner file to run server and organizes server into a class

--- lib/server.rb
+++ lib/server.rb
@@ -1,15 +1,26 @@
 require 'socket'
-tcp_server = TCPServer.new(9292)
-client = tcp_server.accept
 
-response = "<pre>Hello, World!</pre>"
-output = "<html><head></head><body>#{response}</body></html>"
-headers = ["http/1.1 200 ok",
-          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-          "server: ruby",
-          "content-type: text/html; charset=iso-8859-1",
-          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-client.puts headers
-client.puts output
+class Server
+  attr_reader :client
+  def initialize(port = 9292)
+    @port = port
+    @tcp_server = TCPServer.new(port)
+    @client = tcp_server.accept
+    @counter = 0
+  end
 
-client.close
+  def headers
+    ['http/1.1 200 ok',
+     "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+     'server: ruby',
+     'content-type: text/html; charset=iso-8859-1',
+     "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+  end
+
+  def respond
+    output = "Hello, World! (#{counter})"
+    client.puts headers
+    client.puts output
+    @counter += 1
+  end
+end
--- test/server_test.rb
+++ test/server_test.rb
@@ -1,12 +1,21 @@
+require './lib/server'
 require 'faraday'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class ServerTest < Minitest::Test
-  def test_that_server_responds_on_9292_host
-    expected = '<html><head></head><body><pre>Hello, World!</pre></body></html>'
-    response = Faraday.get 'http://localhost:9292'
 
-    assert_equal expected, response.body
+  def test_that_server_class_exists
+    server = Server.new
+
+    assert_instance_of Server, server
+  end
+
+  def test_that_server_responds_on_9292_host
+    response = Faraday.get 'http://localhost:9292'
+    assert_equal 'Hello, World! (0)', response.body
+
+    response = Faraday.get 'http://localhost:9292'
+    assert_equal 'Hello, World! (1)', response.body
   end
 end
